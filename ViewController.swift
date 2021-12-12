6da87a26a62b8e26428f15530f4b3b9704623ebd
jinlongli <38239621+jasonnnnli@users.noreply.github.com>
2021-12-12 10:01:36 -0500

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print(findSecondlargest(arr: arr))
         print(isPalindrome(value: "wawa"))
+        print(mergeSort([1,3,4,7,2]))
+        print(swap(10,5))
         // Do any additional setup after loading the view.
     }
 
@@ -38,5 +40,63 @@ class ViewController: UIViewController {
     {
         return value == String(value.reversed())
     }
+    func mergeSort(_ array: [Int] ) -> [Int] {
+      guard array.count > 1 else { return array }
+
+      let middleIndex = array.count / 2
+      
+      let leftArray = mergeSort(Array(array[0..<middleIndex]))
+      let rightArray = mergeSort(Array(array[middleIndex..<array.count]))
+      
+      return merge(leftArray, rightArray)
+    }
+
+    func merge(_ left: [Int], _ right: [Int]) -> [Int] {
+      var leftIndex = 0
+      var rightIndex = 0
+
+      var orderedArray: [Int] = []
+      
+      while leftIndex < left.count && rightIndex < right.count {
+        let leftElement = left[leftIndex]
+        let rightElement = right[rightIndex]
+
+        if leftElement < rightElement {
+          orderedArray.append(leftElement)
+          leftIndex += 1
+        } else if leftElement > rightElement {
+          orderedArray.append(rightElement)
+          rightIndex += 1
+        } else {
+          orderedArray.append(leftElement)
+          leftIndex += 1
+          orderedArray.append(rightElement)
+          rightIndex += 1
+        }
+      }
+
+      while leftIndex < left.count {
+        orderedArray.append(left[leftIndex])
+        leftIndex += 1
+      }
+
+      while rightIndex < right.count {
+        orderedArray.append(right[rightIndex])
+        rightIndex += 1
+      }
+      
+      return orderedArray
+    }
+    func swap(_ xx : Int , _ yy : Int){
+        var x = xx
+        var y = yy
+
+        x = x + y
+        y = x - y
+        x = x - y
+        print (x,y)
+        
+    }
+
 }
 
